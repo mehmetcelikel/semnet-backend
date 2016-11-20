67bb207ae660029bb43a8d15481564404e35b6d1
mehmetcelikel <mehmetcelikel@gmail.com>
2016-11-21 08:11:52 +1100

    log4j.xml file has been added

--- semnet.services/src/main/java/com/boun/swe/semnet/sevices/config/CacheConfig.java
+++ semnet.services/src/main/java/com/boun/swe/semnet/sevices/config/CacheConfig.java
@@ -1,11 +1,8 @@
 package com.boun.swe.semnet.sevices.config;
 
-import java.lang.reflect.Method;
-
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
