4275feed9b76db8358d9fa503947bc0ab087e2a2
mehmetcelikel <mehmetcelikel@gmail.com>
2016-12-14 08:46:50 +1100

    Test

--- semnet.services/src/main/java/com/boun/swe/semnet/sevices/service/impl/SemanticSearchServiceImpl.java
+++ semnet.services/src/main/java/com/boun/swe/semnet/sevices/service/impl/SemanticSearchServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.boun.swe.semnet.commons.data.request.BasicSearchRequest;
 import com.boun.swe.semnet.commons.data.response.QueryLabelResponse;
+import com.boun.swe.semnet.commons.dbpedia.SPARQLRunner;
 import com.boun.swe.semnet.sevices.db.manager.SemanticTagManager;
 import com.boun.swe.semnet.sevices.service.BaseService;
 import com.boun.swe.semnet.sevices.service.SemanticSearchService;
@@ -21,6 +22,6 @@ public class SemanticSearchServiceImpl extends BaseService implements SemanticSe
 		
 		validate(request);
 		
-		return semanticTagManager.queryLabel(request.getQueryString());
+		return SPARQLRunner.getInstance().runQuery(request.getQueryString());
 	}
 }
