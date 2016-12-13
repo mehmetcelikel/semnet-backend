b0e5aada7a6c18d5bd9014af064b29cca2a9cbcc
mehmetcelikel <mehmetcelikel@gmail.com>
2016-12-14 08:43:46 +1100

    Cacheable annotation commented out

--- semnet.services/src/main/java/com/boun/swe/semnet/sevices/db/manager/impl/SemanticTagManagerImpl.java
+++ semnet.services/src/main/java/com/boun/swe/semnet/sevices/db/manager/impl/SemanticTagManagerImpl.java
@@ -1,6 +1,5 @@
 package com.boun.swe.semnet.sevices.db.manager.impl;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.boun.swe.semnet.commons.data.response.QueryLabelResponse;
@@ -10,7 +9,7 @@ import com.boun.swe.semnet.sevices.db.manager.SemanticTagManager;
 @Service
 public class SemanticTagManagerImpl implements SemanticTagManager{
 
-	@Cacheable(value="semanticTagCache", key="#input")
+//	@Cacheable(value="semanticTagCache", key="#input")
 	@Override
 	public QueryLabelResponse queryLabel(String input){
 		return SPARQLRunner.getInstance().runQuery(input);
