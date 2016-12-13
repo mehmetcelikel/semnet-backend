c9b859f26d203f25c847ee1050ddc8fc1fac1bb9
mehmetcelikel <mehmetcelikel@gmail.com>
2016-12-14 08:53:21 +1100

    Test

--- semnet.services/src/main/java/com/boun/swe/semnet/sevices/db/manager/SemanticTagManager.java
+++
@@ -1,8 +0,0 @@
-package com.boun.swe.semnet.sevices.db.manager;
-
-import com.boun.swe.semnet.commons.data.response.QueryLabelResponse;
-
-public interface SemanticTagManager {
-
-	public QueryLabelResponse queryLabel(String input);
-}
--- semnet.services/src/main/java/com/boun/swe/semnet/sevices/service/impl/SemanticSearchServiceImpl.java
+++ semnet.services/src/main/java/com/boun/swe/semnet/sevices/service/impl/SemanticSearchServiceImpl.java
@@ -1,12 +1,10 @@
 package com.boun.swe.semnet.sevices.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.boun.swe.semnet.commons.data.request.BasicSearchRequest;
 import com.boun.swe.semnet.commons.data.response.QueryLabelResponse;
 import com.boun.swe.semnet.commons.dbpedia.SPARQLRunner;
-import com.boun.swe.semnet.sevices.db.manager.SemanticTagManager;
 import com.boun.swe.semnet.sevices.service.BaseService;
 import com.boun.swe.semnet.sevices.service.SemanticSearchService;
 
@@ -14,9 +12,6 @@ import com.boun.swe.semnet.sevices.service.SemanticSearchService;
 @Service
 public class SemanticSearchServiceImpl extends BaseService implements SemanticSearchService{
 
-	@Autowired
-	SemanticTagManager semanticTagManager;
-	
 	@Override
 	public QueryLabelResponse queryLabel(BasicSearchRequest request){
 		
