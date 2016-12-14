3d026638e341ed0396a29919a7418cda00b27889
mehmetcelikel <mehmetcelikel@gmail.com>
2016-12-15 06:29:05 +1100

    General refactoring

--- semnet.commons/src/main/java/com/boun/swe/semnet/commons/data/response/QueryLabelResponse.java
+++ semnet.commons/src/main/java/com/boun/swe/semnet/commons/data/response/QueryLabelResponse.java
@@ -37,7 +37,11 @@ public class QueryLabelResponse extends ActionResponse{
 		if(dataList == null){
 			dataList = new ArrayList<DataObj>();
 		}
-		dataList.add(new DataObj(label, clazz, description));
+		DataObj obj = new DataObj(label, clazz, description);
+		
+		if(!dataList.contains(obj)){
+			dataList.add(obj);			
+		}
 	}
 	
 	public List<DataObj> getDataList(){
@@ -56,6 +60,37 @@ public class QueryLabelResponse extends ActionResponse{
 			this.label = label;
 			this.clazz = clazz;
 //			this.description = description; 
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			DataObj other = (DataObj) obj;
+			if (clazz == null) {
+				if (other.clazz != null)
+					return false;
+			} else if (!clazz.equals(other.clazz))
+				return false;
+			if (label == null) {
+				if (other.label != null)
+					return false;
+			} else if (!label.equals(other.label))
+				return false;
+			return true;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((clazz == null) ? 0 : clazz.hashCode());
+			result = prime * result + ((label == null) ? 0 : label.hashCode());
+			return result;
 		}
 	}
 	
